f04a81c2685de96d15268009529192b69a8d9bbb
Frank <11590224+frankafan@users.noreply.github.com>
2022-01-26 15:45:38 -0800

    Reformat.

--- nnunet/experiment_planning/nnUNet_convert_imagedrive_data.py
+++ nnunet/experiment_planning/nnUNet_convert_imagedrive_data.py
@@ -9,25 +9,33 @@ def crawl_and_format_data_directory(folder, taskId, taskName):
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith('.nii.gz'):
-                taskFolderName = 'Task' + "{0:03}".format(taskId) + '_' + taskName
+                taskFolderName = 'Task' + \
+                    "{0:03}".format(taskId) + '_' + taskName
                 if '_PT' in file:
-                    fileDir = os.path.join(nnUNet_raw_data, os.path.join(taskFolderName, 'imagesTr'))
+                    fileDir = os.path.join(
+                        nnUNet_raw_data, os.path.join(taskFolderName, 'imagesTr'))
                     if not os.path.exists(fileDir):
                         os.makedirs(fileDir)
-                    os.replace(os.path.join(root, file), os.path.join(fileDir, file.replace('_PT', '').replace('.nii.gz', '_0000.nii.gz')))
+                    os.replace(os.path.join(root, file), os.path.join(
+                        fileDir, file.replace('_PT', '').replace('.nii.gz', '_0000.nii.gz')))
                 elif '_RTSTRUCT' in file:
-                    fileDir = os.path.join(nnUNet_raw_data, os.path.join(taskFolderName, 'labelsTr'))
+                    fileDir = os.path.join(
+                        nnUNet_raw_data, os.path.join(taskFolderName, 'labelsTr'))
                     if not os.path.exists(fileDir):
                         os.makedirs(fileDir)
-                    os.replace(os.path.join(root, file), os.path.join(fileDir, file.replace('_RTSTRUCT', '')))
-            
+                    os.replace(os.path.join(root, file), os.path.join(
+                        fileDir, file.replace('_RTSTRUCT', '')))
+
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(description="Convert imagedrive data and format them to be used in nnU-Net")
+    parser = argparse.ArgumentParser(
+        description="Convert imagedrive data and format them to be used in nnU-Net")
     parser.add_argument("-i", help="Input folder", required=True)
-    parser.add_argument("-t", help="Task ID", default=500, type=int, required=False)
-    parser.add_argument("-n", help="Task name", default="TaskName", required=False)
+    parser.add_argument("-t", help="Task ID", default=500,
+                        type=int, required=False)
+    parser.add_argument("-n", help="Task name",
+                        default="TaskName", required=False)
     args = parser.parse_args()
 
     crawl_and_format_data_directory(args.i, args.t, args.n)
