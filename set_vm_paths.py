280b1a79fd6cca5c7bd2004cf411a92008c77e11
Ubuntu <azureuser@gpuvm00005jhubvm01.2eascjkot3fe5hxtjlyj4zdl0h.ux.internal.cloudapp.net>
2022-02-16 23:53:38 +0000

    Use python to add environment variables on VM.

---
+++ set_vm_paths.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ[
+    "nnUNet_raw_data_base"
+] = "/home/azureuser/nnUNet/nnunet/nnUNet_raw_data_base"
+os.environ[
+    "nnUNet_preprocessed"
+] = "/home/azureuser/nnUNet/nnunet/nnUNet_preprocessed"
+os.environ[
+    "RESULTS_FOLDER"
+] = "/home/azureuser/nnUNet/nnunet/nnUNet_trained_models"
